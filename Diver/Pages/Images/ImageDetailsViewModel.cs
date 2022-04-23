967cf45512e486c8e341aa4735f13b16586121f5
Taras Kizlo <tki@scada-international.com>
2022-04-23 11:52:34 +0300

    Update UI of image history item

--- Diver/Pages/Images/ImageDetailsViewModel.cs
+++ Diver/Pages/Images/ImageDetailsViewModel.cs
@@ -2,8 +2,15 @@
 
 namespace Diver.Pages.Images
 {
+    class ImageHistoryDto
+    {
+        public string Command { get; set; }
+        public string Id { get; set; }
+        public string Size { get; set; }
+    }
     public class ImageDetailsViewModel : ViewModelBase
     {
+
         public ImageDetailsViewModel(NavigationManager navigationManager)
             : base(navigationManager)
         {
